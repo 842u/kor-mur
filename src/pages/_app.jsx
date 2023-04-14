0e430177ed2125814f548585e28a2ac09adc749f
Kamil Bażanow <kamil.bazanow@gmail.com>
2023-04-14 16:42:11 +0200

    initial commit

---
+++ src/pages/_app.jsx
@@ -0,0 +1,16 @@
+import 'normalize.css';
+import '@/styles/globals.css';
+
+// eslint-disable-next-line
+import { Bebas_Neue } from 'next/font/google';
+
+const inter = Bebas_Neue({ weight: '400', subsets: ['latin'] });
+
+export default function App({ Component, pageProps }) {
+  return (
+    <main className={inter.className}>
+      {/* eslint-disable-next-line */}
+      <Component {...pageProps} />;
+    </main>
+  );
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,5 @@
+import Logo from '@/components/Logo/Logo';
+
+export default function Home() {
+  return <Logo />;
+}
