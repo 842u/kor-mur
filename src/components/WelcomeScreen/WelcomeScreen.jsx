d013aa0e8eea8c93e37c8e4f1a7b5eb1f493d3dd
Kamil Bażanow <kamil.bazanow@gmail.com>
2023-04-26 09:43:53 +0200

    add framer-motion

--- src/components/WelcomeScreen/WelcomeScreen.jsx
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -1,10 +1,18 @@
+import { motion } from 'framer-motion';
+
 import CircleLogo from '../CircleLogo/CircleLogo.jsx';
 import styles from './WelcomeScreen.module.scss';
 
 export default function WelcomeScreen() {
   return (
     <div className={styles.loader}>
-      <CircleLogo text="murawska.studio" className={styles['brand-logo']} />
+      <motion.div
+        initial={{ opacity: 0, rotate: 0 }}
+        animate={{ opacity: 1, rotate: 900 }}
+        transition={{ type: 'spring', duration: 4, delay: 0.5 }}
+      >
+        <CircleLogo text="murawska.studio" className={styles['brand-logo']} />
+      </motion.div>
     </div>
   );
 }
