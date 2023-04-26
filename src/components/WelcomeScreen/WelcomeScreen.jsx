d9c426d34332a5ebb0926d737873d6e7f460464e
Kamil Bażanow <kamil.bazanow@gmail.com>
2023-04-26 13:38:29 +0200

    think it work

--- src/components/WelcomeScreen/WelcomeScreen.jsx
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -7,13 +7,12 @@ export default function WelcomeScreen() {
   return (
     <div className={styles.loader}>
       <motion.div
-        initial={{ opacity: 0, scale: 0.5, rotate: 180 }}
-        animate={{ opacity: 1, scale: 1, rotate: 900 }}
+        initial={{ opacity: 0, scale: 0, rotate: 0 }}
+        exit={{ opacity: 1, scale: 1, rotate: 900 }}
         transition={{
           type: 'spring',
-          duration: 2,
-          delay: 0.5,
-          ease: [0, 0.71, 0.2, 1.01],
+          duration: 3,
+          ease: 'circInOut',
         }}
       >
         <CircleLogo text="murawska.studio" className={styles['brand-logo']} />
