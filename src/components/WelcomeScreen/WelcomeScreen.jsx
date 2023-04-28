30ec78edd3d1c96c1b7f5853ffa764141df47109
Kamil Bażanow <kamil.bazanow@gmail.com>
2023-04-28 13:11:06 +0200

    refactor: minimize framer-motion bundle size

--- src/components/WelcomeScreen/WelcomeScreen.jsx
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion';
+import { domAnimation, LazyMotion, m } from 'framer-motion';
 
 import CircleLogo from '../CircleLogo/CircleLogo.jsx';
 import styles from './WelcomeScreen.module.scss';
@@ -6,16 +6,18 @@ import styles from './WelcomeScreen.module.scss';
 export default function WelcomeScreen({ className }) {
   return (
     <div className={`${styles.loader} ${className}`}>
-      <motion.div
-        initial={{ scale: 0, rotate: 0 }}
-        animate={{ scale: 1, rotate: 900 }}
-        transition={{
-          type: 'spring',
-          duration: 2,
-        }}
-      >
-        <CircleLogo text="MURAWSKA.STUDIO" className={styles['brand-logo']} />
-      </motion.div>
+      <LazyMotion features={domAnimation}>
+        <m.div
+          initial={{ scale: 0, rotate: 0 }}
+          animate={{ scale: 1, rotate: 900 }}
+          transition={{
+            type: 'spring',
+            duration: 2,
+          }}
+        >
+          <CircleLogo text="MURAWSKA.STUDIO" className={styles['brand-logo']} />
+        </m.div>
+      </LazyMotion>
     </div>
   );
 }
