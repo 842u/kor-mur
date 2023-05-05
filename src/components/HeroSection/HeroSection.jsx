abba6a0c8a1a97c8c0fd272a75cdaf04de774510
Kamil Bażanow <kamil.bazanow@gmail.com>
2023-05-05 17:16:25 +0200

    refactor: change HeroSection style

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -5,57 +5,63 @@ import heroImg1 from 'public/images/hero/render-4.png';
 import heroImg4 from 'public/images/hero/render-6.png';
 import heroImg2 from 'public/images/hero/render-9.png';
 
+import CircleLogo from '../CircleLogo/CircleLogo';
 import styles from './HeroSection.module.scss';
 
 export default function HeroSection() {
   return (
     <section className={styles['hero-section']}>
-      <Image
-        src={heroImg1}
-        priority
-        fill
-        placeholder="blur"
-        sizes="33vw"
-        className={styles['img-1']}
-        aria-hidden
-        alt=""
-      />
-      <Image
-        src={heroImg2}
-        placeholder="blur"
-        fill
-        sizes="33vw"
-        className={styles['img-2']}
-        aria-hidden
-        alt=""
-      />
-      <Image
-        src={heroImg3}
-        placeholder="blur"
-        fill
-        sizes="33vw"
-        className={styles['img-3']}
-        aria-hidden
-        alt=""
-      />
-      <Image
-        src={heroImg4}
-        placeholder="blur"
-        fill
-        sizes="33vw"
-        className={styles['img-4']}
-        aria-hidden
-        alt=""
-      />
-      <Image
-        src={heroImg5}
-        placeholder="blur"
-        fill
-        sizes="33vw"
-        className={styles['img-5']}
-        aria-hidden
-        alt=""
-      />
+      <div className={styles['logo-container']}>
+        <CircleLogo text="MURAWSKA.STUDIO" className={styles.logo} />
+      </div>
+      <div className={styles['image-container']}>
+        <Image
+          src={heroImg1}
+          priority
+          fill
+          placeholder="blur"
+          sizes="33vw"
+          className={styles['img-1']}
+          aria-hidden
+          alt=""
+        />
+        <Image
+          src={heroImg2}
+          placeholder="blur"
+          fill
+          sizes="33vw"
+          className={styles['img-2']}
+          aria-hidden
+          alt=""
+        />
+        <Image
+          src={heroImg3}
+          placeholder="blur"
+          fill
+          sizes="33vw"
+          className={styles['img-3']}
+          aria-hidden
+          alt=""
+        />
+        <Image
+          src={heroImg4}
+          placeholder="blur"
+          fill
+          sizes="33vw"
+          className={styles['img-4']}
+          aria-hidden
+          alt=""
+        />
+        <Image
+          src={heroImg5}
+          placeholder="blur"
+          fill
+          sizes="33vw"
+          className={styles['img-5']}
+          aria-hidden
+          alt=""
+        />
+      </div>
     </section>
   );
 }
