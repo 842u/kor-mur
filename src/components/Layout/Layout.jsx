df8d0e8f4a6cee3bb976e12b0a5940aa01ecea57
Kamil Bażanow <kamil.bazanow@gmail.com>
2023-05-05 10:48:34 +0200

    refactor: unmount WelcomeScreen after time and move it to Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react';
+
+import WelcomeScreen from '../WelcomeScreen/WelcomeScreen';
 import Footer from './Footer/Footer';
 import NavBar from './NavBar/NavBar';
 
 export default function Layout({ children }) {
+  const [isWelcomeAnimationFinished, setIsWelcomeAnimationFinished] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsWelcomeAnimationFinished(true);
+    }, 5000);
+  }, []);
+
   return (
     <>
+      {!isWelcomeAnimationFinished && <WelcomeScreen />}
+
       <NavBar />
       <main>{children}</main>
       <Footer />
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,9 @@
 import Head from 'next/head';
 
-import WelcomeScreen from '../components/WelcomeScreen/WelcomeScreen';
-import styles from './index.module.scss';
-
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Murawska Studio</title>
-      </Head>
-      <WelcomeScreen className={styles.welcome} />
-      AHA
-    </div>
+    <Head>
+      <title>Murawska Studio</title>
+    </Head>
   );
 }
