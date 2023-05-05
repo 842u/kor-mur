d83824672b2a75c8b103779b1d31430efa5ca524
Kamil Bażanow <kamil.bazanow@gmail.com>
2023-05-05 10:02:24 +0200

    refactor: move WelcomeScreen to Layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,9 @@
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
-
-import WelcomeScreen from '@/components/WelcomeScreen/WelcomeScreen';
 
 export default function Home() {
-  const [isWelcomeAnimationFinished, setIsWelcomeAnimationFinished] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsWelcomeAnimationFinished(true);
-    }, 5000);
-  }, []);
-
   return (
-    <>
-      <Head>
-        <title>Murawska Studio</title>
-      </Head>
-      {!isWelcomeAnimationFinished && <WelcomeScreen />}
-    </>
+    <Head>
+      <title>Murawska Studio</title>
+    </Head>
   );
 }
