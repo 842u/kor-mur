2d6fce439735028472bed8c2332f4da9b4189396
Kamil Bażanow <kamil.bazanow@gmail.com>
2023-05-08 11:48:13 +0200

    feat: add media queries to MottoSection

--- src/components/MottoSection/MottoSection.jsx
+++ src/components/MottoSection/MottoSection.jsx
@@ -9,33 +9,33 @@ import styles from './MottoSection.module.scss';
 export default function MottoSection() {
   return (
     <section className={styles['motto-section']}>
-      <h2 className={`${secondaryFont.className} ${styles.motto}`}>MOTTO</h2>
+      <h2 className={secondaryFont.className}>MOTTO</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, vero voluptatem quam, et,
         atque laborum excepturi deserunt molestias porro unde consequatur inventore totam architecto
         omnis corporis placeat magni voluptas? Magnam?
       </p>
-      <div className={styles.decor}>
+      <div className={styles.addnotation}>
         <div className={styles['image-container']}>
           <Image
             src={mottoImg1}
             placeholder="blur"
-            sizes="(max-width: 810px) 100vw, 40vw"
+            sizes="(max-width: 810px) 100vw, 50vw"
             alt="motto img"
             fill
             className={styles.image}
           />
         </div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, vero voluptatem quam,
-          et, atque laborum excepturi deserunt molestias porro unde consequatur inventore totam
-          architecto omnis corporis placeat magni voluptas? Magnam?
-        </p>
-        <Link href="/about" className={styles.link}>
-          <button type="button" className={styles.button}>
+        <div className={styles.about}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, vero voluptatem quam,
+            et, atque laborum excepturi deserunt molestias porro unde consequatur inventore totam
+            architecto omnis corporis placeat magni voluptas? Magnam?
+          </p>
+          <Link href="/about" className={styles.link}>
             O MNIE
-          </button>
-        </Link>
+          </Link>
+        </div>
       </div>
     </section>
   );
