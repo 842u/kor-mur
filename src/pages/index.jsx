2beaffe56e3e8b146d989a68db55e8bb3042dda2
Kamil Bażanow <kamil.bazanow@gmail.com>
2023-05-10 18:12:50 +0200

    chore: add and configure apollo/client dependency

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+import { gql } from '@apollo/client';
 import Head from 'next/head';
 
 import HeroSection from '@/components/HeroSection/HeroSection';
@@ -5,7 +6,11 @@ import Layout from '@/components/Layout/Layout';
 import MottoSection from '@/components/MottoSection/MottoSection';
 import ProjectsSection from '@/components/ProjectsSection/ProjectsSection';
 
-export default function Home() {
+import apolloClient from '../../graphql/apolloClient';
+
+export default function Home({ data }) {
+  console.log(data);
+
   return (
     <>
       <Head>
@@ -18,4 +23,22 @@ export default function Home() {
       </Layout>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const { data } = await apolloClient.query({
+    query: gql`
+      query RootQuery {
+        allProject {
+          name
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      data,
+    },
+  };
 }
