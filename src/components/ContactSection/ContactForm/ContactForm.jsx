5a83d0fb2421e4ce429dce50023ecad259281684
Kamil Bażanow <kamil.bazanow@gmail.com>
2023-05-15 13:59:59 +0200

    refactor: change validation feedback logic

--- src/components/ContactSection/ContactForm/ContactForm.jsx
+++ src/components/ContactSection/ContactForm/ContactForm.jsx
@@ -1,51 +1,7 @@
-import { useDeferredValue, useMemo, useState } from 'react';
-
-import getValidationInfo from '@/utils/validation';
-
 import styles from './ContactForm.module.scss';
 import FormField from './FormField/FormField';
 
 export default function ContactForm() {
-  const [enteredName, setEnteredName] = useState('');
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredTelephone, setEnteredTelephone] = useState('');
-  const [enteredMessage, setEnteredMessage] = useState('');
-
-  const deferredName = useDeferredValue(enteredName);
-  const deferredEmail = useDeferredValue(enteredEmail);
-  // const deferredTelephone = useDeferredValue(enteredTelephone);
-  // const deferredMessage = useDeferredValue(enteredMessage);
-
-  const nameValidationInfo = useMemo(() => {
-    return getValidationInfo({ value: deferredName, type: 'text', minLength: 5, maxLength: 30 });
-  }, [deferredName]);
-
-  const emailValidationInfo = useMemo(() => {
-    return getValidationInfo({ value: deferredEmail, type: 'email', minLength: 3, maxLength: 30 });
-  }, [deferredEmail]);
-
-  const inputChangeHandler = (event) => {
-    const { id, value } = event.target;
-    console.log(id, value);
-
-    switch (id) {
-      case 'name':
-        setEnteredName(value);
-        break;
-      case 'email':
-        setEnteredEmail(value);
-        break;
-      case 'telephone':
-        setEnteredTelephone(value);
-        break;
-      case 'message':
-        setEnteredMessage(value);
-        break;
-      default:
-        break;
-    }
-  };
-
   const formSubmitHandler = (event) => {
     event.preventDefault();
   };
@@ -60,40 +16,6 @@ export default function ContactForm() {
         required
         minLength="2"
         maxLength="30"
-        value={enteredName}
-        validationInfo={nameValidationInfo}
-        onChange={inputChangeHandler}
-      />
-      <FormField
-        label="Email"
-        type="email"
-        id="email"
-        placeholder="Enter your email"
-        required
-        minLength="3"
-        maxLength="256"
-        value={enteredEmail}
-        validationInfo={emailValidationInfo}
-        onChange={inputChangeHandler}
-      />
-      <FormField
-        label="Telephone"
-        type="tel"
-        id="telephone"
-        placeholder="Enter your telephone"
-        maxLength="15"
-        value={enteredTelephone}
-        onChange={inputChangeHandler}
-      />
-      <FormField
-        label="Message"
-        type="textarea"
-        id="message"
-        placeholder="Enter your message"
-        required
-        maxLength="500"
-        value={enteredMessage}
-        onChange={inputChangeHandler}
       />
       <button type="submit">Send</button>
     </form>
--- src/components/ContactSection/ContactForm/FormField/FormField.jsx
+++ src/components/ContactSection/ContactForm/FormField/FormField.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import styles from './FormField.module.scss';
 
 export default function FormField({
@@ -5,21 +7,56 @@ export default function FormField({
   type,
   id,
   placeholder,
-  value,
   required = false,
   minLength,
   maxLength,
-  onChange,
-  validationInfo,
 }) {
+  const [fieldValue, setValue] = useState('');
+  const [isTouched, setIsTouched] = useState(false);
+  const [hasError, setHasError] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const valueChangeHandler = (event) => {
+    setValue(event.target.value);
+    if (required && event.target.value.trim().length === 0) {
+      setHasError(true);
+      setErrorMessage('This field is required');
+    } else if (event.target.value.trim().length < minLength) {
+      setHasError(true);
+      setErrorMessage(`This field must be at least ${minLength} characters long`);
+    } else if (event.target.value.trim().length > maxLength) {
+      setHasError(true);
+      setErrorMessage(`This field must be no more than ${maxLength} characters long`);
+    } else {
+      setHasError(false);
+    }
+  };
+
+  const isTouchedHandler = (event) => {
+    setIsTouched(true);
+    if (required && event.target.value.trim().length === 0) {
+      setHasError(true);
+      setErrorMessage('This field is required');
+    } else if (event.target.value.trim().length < minLength) {
+      setHasError(true);
+      setErrorMessage(`This field must be at least ${minLength} characters long`);
+    } else if (event.target.value.trim().length > maxLength) {
+      setHasError(true);
+      setErrorMessage(`This field must be no more than ${maxLength} characters long`);
+    } else {
+      setHasError(false);
+    }
+  };
+
   const textareaField = (
     <textarea
       id={id}
       name={id}
       placeholder={placeholder}
-      value={value}
+      value={fieldValue}
       required={required}
-      onChange={onChange}
+      onChange={valueChangeHandler}
+      onBlur={isTouchedHandler}
     />
   );
 
@@ -29,26 +66,30 @@ export default function FormField({
       id={id}
       name={id}
       placeholder={placeholder}
-      value={value}
+      value={fieldValue}
       aria-label={`${id} input field`}
       required={required}
       minLength={minLength}
       maxLength={maxLength}
-      onChange={onChange}
+      onChange={valueChangeHandler}
+      onBlur={isTouchedHandler}
     />
   );
 
-  const isValid = validationInfo?.isValid;
-  const validationErrorMessage = validationInfo?.errorMessage;
+  const requiredFlag = <span className={styles['required-flag']}> * </span>;
+
+  const fieldClasses = `${styles.field} ${isTouched && hasError && styles.invalid} ${
+    isTouched && !hasError && styles.valid
+  }`;
 
   return (
-    <label
-      className={`${styles['field-label']} ${isValid ? styles.valid : styles.invalid}`}
-      htmlFor={id}
-    >
-      {`${required ? '* ' : ''}${label}:`}
+    <label className={fieldClasses} htmlFor={id}>
+      <span>
+        {label}
+        {required && requiredFlag}
+      </span>
       {type === 'textarea' ? textareaField : inputField}
-      {validationErrorMessage && <p>{validationErrorMessage}</p>}
+      {isTouched && hasError && <p>{errorMessage}</p>}
     </label>
   );
 }
