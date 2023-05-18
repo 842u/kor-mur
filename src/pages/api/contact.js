427da9675a1bde7c5cff23c01f0b42f5dc0f35de
Kamil Bażanow <kamil.bazanow@gmail.com>
2023-05-18 13:09:22 +0200

    refactor: add response messages

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -4,6 +4,11 @@ sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // eslint-disable-next-line
 export default async function sendMail(req, res) {
+  if (req.method !== 'POST') {
+    res.status(400).json({ message: 'Only POST requests allowed' });
+    return;
+  }
+
   console.log(req.body);
 
   const mail = {
@@ -11,13 +16,15 @@ export default async function sendMail(req, res) {
     from: 'kontakt@murawska.studio',
     subject: 'Sending with SendGrid is Fun',
     text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    html: '<strong>test</strong>',
   };
 
   try {
     const response = await sendgridMail.send(mail);
     console.log(response);
+    res.status(200).json({ message: 'email send' });
   } catch (error) {
     console.error(error);
+    res.status(400).json({ message: `${error.message}` });
   }
 }
