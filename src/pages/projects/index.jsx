9078bc42dc5eebf96c7aa81c0330d15573ae209f
Kamil Bażanow <kamil.bazanow@gmail.com>
2023-05-24 18:22:47 +0200

    feat: get all projects data

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,5 +1,29 @@
-export default function Projects() {
-  return <h1>Projects Page</h1>;
+import ProjectCard from '@/components/ProjectCard/ProjectCard';
+
+import apolloClient from '../../../graphql/apolloClient';
+import GET_ALL_PROJECTS_DATA from '../../../graphql/queryAllProjectsData';
+
+export default function Projects({ projects }) {
+  console.log(projects);
+
+  return (
+    <>
+      <h1>Projects Page</h1>
+      {projects.map((project) => (
+        <ProjectCard key={project._id} project={project} sizes="100vw" />
+      ))}
+    </>
+  );
 }
 
-export async function getStaticProps() {}
+export async function getStaticProps() {
+  const { data } = await apolloClient.query({ query: GET_ALL_PROJECTS_DATA });
+
+  const projects = data.allProject;
+
+  return {
+    props: {
+      projects,
+    },
+  };
+}
