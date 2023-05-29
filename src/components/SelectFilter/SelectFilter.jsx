d03d97f2c84796d9f5c6fc73eaed9b4f583d14c0
Kamil <kamil.bazanow@gmail.com>
2023-05-29 09:37:48 +0200

    refactor: add defaultValue to select element instead of selected prop in option element

--- src/components/SelectFilter/SelectFilter.jsx
+++ src/components/SelectFilter/SelectFilter.jsx
@@ -4,8 +4,8 @@ export default function SelectFilter({ options }) {
   return (
     <label htmlFor="category">
       choose category
-      <select className={styles.dropdown} id="category" name="category">
-        <option selected aria-label="Filter by all" value="all">
+      <select className={styles.dropdown} defaultValue="all" id="category" name="category">
+        <option aria-label="Filter by all" value="all">
           All
         </option>
         {options.map((option) => (
