fc0ba816ffd94bffd88e05b28b92da53792c04a9
Kamil Bażanow <kamil.bazanow@gmail.com>
2023-05-30 17:55:04 +0200

    refactor: change tests for MottoSection

--- src/components/sections/MottoSection/MottoSection.jsx
+++ src/components/sections/MottoSection/MottoSection.jsx
@@ -1,16 +1,18 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import placeholderImage from 'public/default-image-placeholder.png';
 
 import { secondaryFont } from '@/utils/fonts';
 
+import getDefaultMottoSectionSettings from './getDefaultMottoSectionSettings';
 import styles from './MottoSection.module.scss';
 
+const defaultSettings = getDefaultMottoSectionSettings();
+
 export default function MottoSection({ mottoSectionSettings }) {
-  const mottoTitle = mottoSectionSettings?.title || 'Default motto title';
-  const mottoText = mottoSectionSettings?.text || 'Default motto text';
-  const mottoImageSrc = mottoSectionSettings?.image?.asset?.url || placeholderImage;
-  const mottoDescription = mottoSectionSettings?.description || 'Default motto description';
+  const mottoTitle = mottoSectionSettings?.title || defaultSettings.title;
+  const mottoText = mottoSectionSettings?.text || defaultSettings.text;
+  const mottoImageSrc = mottoSectionSettings?.image?.asset?.url || defaultSettings.image;
+  const mottoDescription = mottoSectionSettings?.description || defaultSettings.description;
 
   return (
     <section className={styles['motto-section']}>
@@ -20,7 +22,7 @@ export default function MottoSection({ mottoSectionSettings }) {
         <div className={styles['image-container']}>
           <Image
             fill
-            alt="motto img"
+            alt="motto image"
             className={styles.image}
             sizes="(max-width: 810px) 100vw, 50vw"
             src={mottoImageSrc}
---
+++ src/components/sections/MottoSection/getDefaultMottoSectionSettings.js
@@ -0,0 +1,10 @@
+import placeholderImage from 'public/default-image-placeholder.png';
+
+export default function getDefaultMottoSectionSettings() {
+  return {
+    title: 'Default motto title',
+    text: 'Default motto text',
+    description: 'Default motto description',
+    image: placeholderImage,
+  };
+}
