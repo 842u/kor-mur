1de6e9931cbe8900cb6d3c3c36e6d50d17098e5e
Kamil Bażanow <kamil.bazanow@gmail.com>
2023-05-30 11:23:46 +0200

    refacotr: remove wrapper main element

--- src/components/layouts/DefaultLayout.jsx
+++ src/components/layouts/DefaultLayout.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import Footer from '@/components/ui/Footer/Footer';
 import NavBar from '@/components/ui/NavBar/NavBar';
 import WelcomeScreen from '@/components/ui/WelcomeScreen/WelcomeScreen';
+import { primaryFont } from '@/utils/fonts';
 
 export default function DefaultLayout({ children }) {
   const [isWelcomeAnimationFinished, setIsWelcomeAnimationFinished] = useState(false);
@@ -15,11 +16,11 @@ export default function DefaultLayout({ children }) {
 
   return (
     <>
-      {!isWelcomeAnimationFinished && <WelcomeScreen />}
+      {!isWelcomeAnimationFinished && <WelcomeScreen className={primaryFont.className} />}
 
-      <NavBar />
-      <main>{children}</main>
-      <Footer />
+      <NavBar className={primaryFont.className} />
+      <main className={primaryFont.className}>{children}</main>
+      <Footer className={primaryFont.className} />
     </>
   );
 }
