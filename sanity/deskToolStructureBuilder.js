3b82fffb398e555241710699b139f41ac94fc21c
Kamil <kamil.bazanow@gmail.com>
2023-05-30 13:37:49 +0200

    feat: add singleton document to sanity studio desk

--- sanity/deskToolStructureBuilder.js
+++ sanity/deskToolStructureBuilder.js
@@ -1,7 +1,16 @@
-const deskToolStructureBuilder = (s) =>
-  s
-    .list()
+import { MasterDetailIcon } from '@sanity/icons';
+
+const deskToolStructureBuilder = (S) =>
+  S.list()
     .title('Base')
-    .items([...s.documentTypeListItems().reverse()]);
+    .items([
+      S.listItem()
+        .title('Motto Section Settings')
+        .icon(MasterDetailIcon)
+        .child(S.document().schemaType('mottoSectionSettings').documentId('mottoSectionSettings')),
+      ...S.documentTypeListItems().filter(
+        (listItem) => !['mottoSectionSettings'].includes(listItem.getId())
+      ),
+    ]);
 
 export default deskToolStructureBuilder;
--- sanity/schema/mottoSectionSettings.js
+++ sanity/schema/mottoSectionSettings.js
@@ -1,21 +1,25 @@
+import { defineField, defineType } from 'sanity';
+
 /**
  * ! Remember to deploy graphql api after changing any schemas
  */
 
-export default {
+const mottoSectionSettings = defineType({
   name: 'mottoSectionSettings',
   title: 'Motto Section Settings',
   type: 'document',
   fields: [
-    {
+    defineField({
       name: 'title',
       title: 'Motto Title',
       type: 'string',
-    },
-    {
+    }),
+    defineField({
       name: 'description',
       title: 'Motto Description',
       type: 'text',
-    },
+    }),
   ],
-};
+});
+
+export default mottoSectionSettings;
