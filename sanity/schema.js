5dc6dfe4426eea6740db3233524c0171d337270b
Kamil <kamil.bazanow@gmail.com>
2023-05-31 10:51:02 +0200

    feat: fetch data for FeaturedPorjectsSection from sanity cms

--- sanity/schema.js
+++ sanity/schema.js
@@ -2,13 +2,13 @@
  * ! Remember to deploy graphql api after changing any schemas
  */
 
-import featuredProjects from './schema/featuredProjects';
+import featuredProjectsSectionSettings from './schema/featuredProjectsSectionSettings';
 import mottoSectionSettings from './schema/mottoSectionSettings';
 import project from './schema/project';
 import tag from './schema/tag';
 
 const schema = {
-  types: [project, featuredProjects, tag, mottoSectionSettings],
+  types: [project, tag, mottoSectionSettings, featuredProjectsSectionSettings],
 };
 
 export default schema;
---
+++ src/components/sections/FeaturedProjectsSection/getDefaultFeaturedProjectsSectionSettings.js
@@ -0,0 +1,6 @@
+export default function getDefaultFeaturedProjectsSettings() {
+  return {
+    title: 'Default featured projects title',
+    description: 'Default featured projects description',
+  };
+}
