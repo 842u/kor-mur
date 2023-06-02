43a40532beb6654f8b613d9da01f7aed6543b037
Kamil <kamil.bazanow@gmail.com>
2023-06-02 10:46:34 +0200

    refactor: query new fields in queryProjectDataBySlug

--- graphql/queryProjectDataBySlug.js
+++ graphql/queryProjectDataBySlug.js
@@ -3,13 +3,27 @@ import { gql } from '@apollo/client';
 const GET_PROJECT_DATA_BY_SLUG = gql`
   query ProjectBySlug($where: ProjectFilter) {
     allProject(where: $where) {
-      _id
       name
       description
+      year
+      location
+      area
+      budget
       mainImage {
         asset {
           url
         }
+      }
+      images {
+        asset {
+          url
+        }
+      }
+      tags {
+        name
+      }
+      slug {
+        current
       }
     }
   }
