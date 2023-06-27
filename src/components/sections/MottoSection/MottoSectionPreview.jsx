d65a12ee6213604533b2cf4a5cd917c9045dd725
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-06-27 09:49:37 +0200

    refactor: add usePreview hook to MottoSectionPreview

--- src/components/sections/MottoSection/MottoSectionPreview.jsx
+++ src/components/sections/MottoSection/MottoSectionPreview.jsx
@@ -1,11 +1,15 @@
 import Link from 'next/link';
 
+import usePreview from '@/../sanity/lib/preview';
+
 import MottoSection from './MottoSection';
 
-export default function MottoSectionPreview({ mottoSectionSettings }) {
+export default function MottoSectionPreview({ query }) {
+  const { data } = usePreview(null, query);
+
   return (
     <>
-      <MottoSection mottoSectionSettings={mottoSectionSettings} />
+      <MottoSection mottoSectionSettings={data} />
       <Link href="/api/disable-draft">Exit Preview Mode</Link>
     </>
   );
