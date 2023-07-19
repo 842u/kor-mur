915372beb3812c806ffac0cfcadb874538d65157
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-07-19 17:09:25 +0200

    working hero image carousel needs perf test

--- src/components/sections/HeroSection/HeroSectionDefault.jsx
+++ src/components/sections/HeroSection/HeroSectionDefault.jsx
@@ -1,4 +1,6 @@
+/* eslint no-return-assign: 0, no-param-reassign: 0, no-plusplus: 0 */
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 import getHeroSectionSetup from './getHeroSectionSetup';
 import styles from './HeroSectionDefault.module.scss';
@@ -6,22 +8,45 @@ import styles from './HeroSectionDefault.module.scss';
 export default function HeroSectionDefault({ settings }) {
   const { heroSectionText, heroSectionImages } = getHeroSectionSetup(settings);
 
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const imageDelay = setTimeout(() => {
+      setCurrentImageIndex((currentIndex) =>
+        currentIndex >= heroSectionImages.length - 1 ? 0 : (currentIndex += 1)
+      );
+    }, 5000);
+
+    return () => {
+      clearTimeout(imageDelay);
+    };
+  }, [currentImageIndex, settings]);
+
   return (
     <section className={styles['hero-section']}>
-      <p>{heroSectionText}</p>
+      <p className={styles['hero-text']}>
+        {heroSectionText}
+        {currentImageIndex}
+      </p>
       <div className={styles['image-container']}>
-        {heroSectionImages.map((image) => (
-          <Image
-            key={image.asset._id}
-            aria-hidden
-            fill
-            priority
-            alt=""
-            className={styles.image}
-            sizes="100vw"
-            src={image.asset.url}
-          />
-        ))}
+        {heroSectionImages.map((image, index) => {
+          const imageStyle = `${styles.image} ${
+            currentImageIndex === index && styles['image--active']
+          }`;
+
+          return (
+            <Image
+              key={image.asset._id}
+              aria-hidden
+              fill
+              alt=""
+              className={imageStyle}
+              priority={index === 0}
+              sizes="100vw"
+              src={image.asset.url}
+            />
+          );
+        })}
       </div>
     </section>
   );
