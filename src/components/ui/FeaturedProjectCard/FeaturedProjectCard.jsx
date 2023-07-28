827fc936eda81153619e78b412d97028e515b8cf
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-07-28 15:10:51 +0200

    css cleanup

--- src/components/ui/FeaturedProjectCard/FeaturedProjectCard.jsx
+++ src/components/ui/FeaturedProjectCard/FeaturedProjectCard.jsx
@@ -6,16 +6,19 @@ import styles from './FeaturedProjectCard.module.scss';
 
 export default function FeaturedProjectCard({ project }) {
   return (
-    <div className={styles.card}>
-      <div aria-label="Project Information" className={styles.label} role="region">
+    <div className={styles['project-card']}>
+      <div aria-label="Project Information" className={styles['project-info']} role="region">
         <h3>{project.name}</h3>
+
         {project.tags?.map((tag) => (
           <TagLabel tag={tag} />
         ))}
+
         <p>{project.descriptionFirst}</p>
       </div>
+
       <Link className={styles['project-link']} href={`/projects/${project.slug.current}`}>
-        <Image fill className={styles['background-image']} src={project.mainImage.asset.url} />
+        <Image fill className={styles['project-image']} src={project.mainImage.asset.url} />
       </Link>
     </div>
   );
