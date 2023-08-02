0c8801227a151063401d79a7d82136cad908d789
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-08-02 12:06:13 +0200

    feat: add 404 page

---
+++ src/pages/404/index.jsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+import styles from './index.module.scss';
+
+export default function Custom404() {
+  return (
+    <section className={styles['page-404']}>
+      <h1>404 - Nie odnaleziono strony.</h1>
+      <p>
+        Przepraszamy ale strona której szukasz nie została odnaleziona. Być może nazwa została
+        zmieniona, usunięta lub jest tymczasowo niedostępna. <br /> ☹️
+      </p>
+      <ul className={styles.instructions}>
+        <li>Upewnij się, że podany adres URL jest prawidłowy.</li>
+        <li>
+          Wróć na <Link href="/">stronę główną</Link> i stamtąd wyszukaj interesującą Cię stronę.
+        </li>
+        <li>
+          <Link href="/#contact">Skontaktuj się</Link> z nami jeśli potrzebujesz pomocy.
+        </li>
+      </ul>
+    </section>
+  );
+}
