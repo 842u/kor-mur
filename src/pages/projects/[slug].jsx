06390eb373d2645cc9110a41b56df7214449dfe9
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-08-03 17:27:46 +0200

    wip test

--- src/pages/projects/[slug].jsx
+++ src/pages/projects/[slug].jsx
@@ -1,19 +1,47 @@
-import { useContext, useEffect } from 'react';
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useCallback, useContext, useEffect } from 'react';
 
 import ProjectSection from '@/components/sections/ProjectSection/ProjectSection';
 import DraftModeContext from '@/context/DraftModeContext';
 
 import getGqlAllProjectsSlugsData from '../../../graphql/queryAllProjectsSlugs';
 import getGqlProjecBySlugData from '../../../graphql/queryProjectDataBySlug';
+import groqQueryProjectBySlug from '../../../groq/queryProjectBySlug';
+
+const DraftProvider = dynamic(() => import('@/components/providers/DraftProvider/DraftProvider'), {
+  loading: () => <p>Loading...</p>,
+});
 
 export default function ProjectPage({ draftMode, project }) {
   const { setIsDraftMode } = useContext(DraftModeContext);
+  const router = useRouter();
+
+  const querySlug = router.query;
+
+  const renderItem = useCallback((draftData) => <ProjectSection data={draftData} />, []);
 
   useEffect(() => {
     setIsDraftMode(draftMode);
   }, []);
 
-  return <ProjectSection settings={project} />;
+  return (
+    <>
+      <Head>
+        <title>Project Page</title>
+      </Head>
+      {draftMode ? (
+        <DraftProvider
+          query={groqQueryProjectBySlug}
+          queryParams={querySlug}
+          renderItem={renderItem}
+        />
+      ) : (
+        <ProjectSection data={project} />
+      )}
+    </>
+  );
 }
 
 export async function getStaticPaths() {
