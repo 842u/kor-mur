94d8bae2512ad37930057b330c0ed6c492a1e72f
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-08-08 19:00:21 +0200

    fix: <head> <title> remove multiple text nodes

--- src/pages/about/HeadAboutPage.jsx
+++ src/pages/about/HeadAboutPage.jsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 export default function HeadAboutPage() {
   return (
     <Head>
-      <title>Murawska Studio - O mnie</title>
+      <title key="title">Murawska Studio - O mnie</title>
       <meta key="description" content="Cześć! Jestem Kornelia." name="description" />
     </Head>
   );
--- src/pages/projects/HeadProjectPage.jsx
+++ src/pages/projects/HeadProjectPage.jsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 export default function HeadProjectPage({ project }) {
   return (
     <Head>
-      <title>Projekt - {project?.[0]?.name}</title>
+      <title key="title">{`Projekt - ${project?.[0]?.name}`}</title>
       <meta
         key="description"
         content={`Tutaj znajdziesz opis mojego projektu ${project?.[0]?.name}.`}
