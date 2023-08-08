1ec0e781e11a1d51d173c1cff2b719501dc3eca8
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-08-08 13:04:01 +0200

    feat: add FeaturedProjectInfo test file

--- src/components/ui/FeaturedProjectCard/FeaturedProjectInfo/FeaturedProjectInfo.test.jsx
+++ src/components/ui/FeaturedProjectCard/FeaturedProjectInfo/FeaturedProjectInfo.test.jsx
@@ -1,18 +1,42 @@
 import '@testing-library/jest-dom';
 
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 
 import { getDefaultFeaturedProjectSetup } from '../getFeaturedProjectSetup';
 import FeaturedProjectInfo from './FeaturedProjectInfo';
 
 const defaultFeaturedProject = getDefaultFeaturedProjectSetup();
 
+jest.mock('next/router', () => ({
+  useRouter: () => ({
+    query: {
+      slug: 'default-tag-slug',
+    },
+  }),
+}));
+
 describe('FeaturedProjectInfo', () => {
-  it('should render heading with project name', () => {
+  it('should render heading with project name', async () => {
     render(<FeaturedProjectInfo project={defaultFeaturedProject} />);
 
     const heading = screen.getByRole('heading', { name: defaultFeaturedProject.name });
 
-    expect(heading).toBeInTheDocument();
+    await waitFor(() => expect(heading).toBeInTheDocument());
+  });
+
+  it('should render TagContainer', async () => {
+    render(<FeaturedProjectInfo project={defaultFeaturedProject} />);
+
+    const tagContainer = screen.getByTestId('tag-container');
+
+    await waitFor(() => expect(tagContainer).toBeInTheDocument());
+  });
+
+  it('should render project first description', async () => {
+    render(<FeaturedProjectInfo project={defaultFeaturedProject} />);
+
+    const description = screen.getByText(defaultFeaturedProject.descriptionFirst);
+
+    await waitFor(() => expect(description).toBeInTheDocument());
   });
 });
