b304dbd96327b843701dcb33324d4ceca1684b92
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-08-15 14:13:37 +0200

    feaT: create InViewSlide and use it to animate MottoSection

---
+++ src/components/animations/InViewSlide.jsx
@@ -0,0 +1,53 @@
+/* eslint react/jsx-props-no-spreading: 0 */
+import { easeInOut, motion } from 'framer-motion';
+
+function animationSetup(slideFrom, offset, duration, ease) {
+  switch (slideFrom) {
+    case 'top':
+      return {
+        initial: { opacity: 0, y: `-${offset}` },
+        transition: { duration, ease },
+        whileInView: { opacity: 1, y: '0px' },
+      };
+
+    case 'right':
+      return {
+        initial: { opacity: 0, x: offset },
+        transition: { duration, ease },
+        whileInView: { opacity: 1, x: '0px' },
+      };
+
+    case 'bottom':
+      return {
+        initial: { opacity: 0, y: offset },
+        transition: { duration, ease },
+        whileInView: { opacity: 1, y: '0px' },
+      };
+
+    case 'left':
+      return {
+        initial: { opacity: 0, x: `-${offset}` },
+        transition: { duration, ease },
+        whileInView: { opacity: 1, x: '0px' },
+      };
+
+    default:
+      return null;
+  }
+}
+export function InViewSlide({
+  children,
+  slideFrom,
+  offset,
+  className,
+  duration = 0.5,
+  ease = easeInOut,
+}) {
+  const style = `${className}`;
+
+  return (
+    <motion.div {...animationSetup(slideFrom, offset, duration, ease)} className={style}>
+      {children}
+    </motion.div>
+  );
+}
