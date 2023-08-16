dc6d5ebf113e052a4c49062626a627d855e5e88f
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-08-16 10:27:40 +0200

    fix: change key prop in FeaturedProjectsSection

--- src/components/sections/FeaturedProjectsSection/FeaturedProjectsSection.jsx
+++ src/components/sections/FeaturedProjectsSection/FeaturedProjectsSection.jsx
@@ -7,12 +7,13 @@ export default function FeaturedProjectsSection({ projects }) {
   return (
     <section className={styles['projects-section']}>
       {projects?.map((project, index) => (
-        <InViewSlide duration={0.5} offset="300px" slideFrom={index % 2 ? 'left' : 'right'}>
-          <FeaturedProjectCard
-            key={project._id}
-            className={styles['project-card']}
-            project={project}
-          />
+        <InViewSlide
+          key={project._id}
+          duration={0.5}
+          offset="300px"
+          slideFrom={index % 2 ? 'left' : 'right'}
+        >
+          <FeaturedProjectCard className={styles['project-card']} project={project} />
         </InViewSlide>
       ))}
     </section>
