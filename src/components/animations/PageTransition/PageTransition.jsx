3ff175c5096a383b7439ae3dc6f0b8cb57b653c5
Kamil Bazanow <kamil.bazanow@gmail.com>
2026-02-23 11:09:46 +0100

    refactor(_app): use preventCssRemoval()

--- src/components/animations/PageTransition/PageTransition.jsx
+++ src/components/animations/PageTransition/PageTransition.jsx
@@ -5,6 +5,8 @@ import { primaryFont } from '@/utils/fonts';
 
 import styles from './PageTransition.module.scss';
 
+export const PAGE_TRANSITION_DURATION = 300;
+
 export function PageTransition({ children }) {
   const router = useRouter();
 
@@ -15,21 +17,21 @@ export function PageTransition({ children }) {
         window?.scrollTo(0, 0);
       }}
     >
-      <motion.main key={router.pathname} className={primaryFont.className}>
+      <motion.main key={router.asPath} className={primaryFont.className}>
         {children}
         <motion.div
           animate={{ scaleY: 0 }}
           className={styles['slide-in']}
           exit={{ scaleY: 1 }}
           initial={{ scaleY: 0 }}
-          transition={{ duration: 0.3, ease: circOut }}
+          transition={{ duration: PAGE_TRANSITION_DURATION / 1000, ease: circOut }}
         />
         <motion.div
           animate={{ scaleY: 0 }}
           className={styles['slide-out']}
           exit={{ scaleY: 0 }}
           initial={{ scaleY: 1 }}
-          transition={{ duration: 0.3, ease: circIn }}
+          transition={{ duration: PAGE_TRANSITION_DURATION / 1000, ease: circIn }}
         />
       </motion.main>
     </AnimatePresence>
